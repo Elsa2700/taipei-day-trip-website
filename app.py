43bcde7f4b03e9a4cbdf7cc31bc1f49d81e00533
elsa <elsa2700@gmail.com>
2021-05-08 14:59:01 +0800

    完成網站景點頁面的 RWD 版面，包括訂購導覽中的時段選擇，以及輪播效果

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 #資料庫連線
 mydb = mysql.connector.connect(
     host="localhost",    #主機名稱
-    user="debian-sys-maint",         #帳號
+    user="debian-sys-maint",  #帳號
     password="XI9BNrhAuluqvv1k", #密碼
     database="travel",     #使用資料庫
 )
@@ -145,6 +145,5 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
-
  
 app.run(host="0.0.0.0", port=3000, debug=True)
