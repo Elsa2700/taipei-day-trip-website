23f3c1827534ff2996216e1d3906bc1105986364
elsa <elsa2700@gmail.com>
2021-05-15 01:01:57 +0800

    註冊問題，修正mysql並更新密碼

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app.secret_key='secret'
 #資料庫連線
 mydb = mysql.connector.connect(
     host="localhost",    #主機名稱
-    user="root",  #帳號
-    password="ELSA2700", #密碼
+    user="debian-sys-maint",  #帳號
+    password="XI9BNrhAuluqvv1k", #密碼
     database="travel",     #使用資料庫
 )
 
@@ -326,5 +326,5 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-# host="0.0.0.0", 
-app.run(port=3000, debug = True)
+
+app.run(host="0.0.0.0", port=3000, debug = True)
